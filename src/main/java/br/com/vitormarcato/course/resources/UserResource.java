b9f036c5dc942f12d16ac871e9fc9db0794a822d
Vitor Marcato <vitor.marcatof@gmail.com>
2022-10-05 14:33:07 -0300

    user entity and resource

---
+++ src/main/java/br/com/vitormarcato/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package br.com.vitormarcato.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.vitormarcato.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Joao", "joao@email.com", "9999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}

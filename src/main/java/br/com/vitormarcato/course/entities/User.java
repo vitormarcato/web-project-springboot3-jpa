678596c64402361b1810f2eca07ba42d20f48d94
Vitor Marcato <vitor.marcatof@gmail.com>
2022-10-17 15:24:47 -0300

    Order table

--- src/main/java/br/com/vitormarcato/course/entities/User.java
+++ src/main/java/br/com/vitormarcato/course/entities/User.java
@@ -1,20 +1,23 @@
 package br.com.vitormarcato.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable{
+public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
@@ -22,6 +25,9 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy= "client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 
@@ -74,6 +80,10 @@ public class User implements Serializable{
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override

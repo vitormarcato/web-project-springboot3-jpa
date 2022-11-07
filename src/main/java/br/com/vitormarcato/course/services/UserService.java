30bbe88d78a061921e9513e7e1121d7f81061f24
Vitor Marcato <vitor.marcatof@gmail.com>
2022-11-07 14:56:32 -0300

    exception handling  - update

--- src/main/java/br/com/vitormarcato/course/services/UserService.java
+++ src/main/java/br/com/vitormarcato/course/services/UserService.java
@@ -12,6 +12,7 @@ import br.com.vitormarcato.course.entities.User;
 import br.com.vitormarcato.course.repositories.UserRepository;
 import br.com.vitormarcato.course.services.exceptions.DatabaseException;
 import br.com.vitormarcato.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -43,9 +44,15 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
+		
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		
+		}catch (EntityNotFoundException e) { 
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
